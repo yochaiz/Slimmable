987b54d56c6faa9e74b72979830d6006b724d7dc
brianchmiel <pancho2787>
2018-12-07 12:03:43 +0200

    * functions for building email with data

--- utils/email.py
+++ utils/email.py
@@ -1,58 +1,10 @@
-from os import path, listdir, makedirs, walk
-from zipfile import ZipFile, ZIP_DEFLATED
-from inspect import getfile, currentframe
+from os import path, listdir
 from smtplib import SMTP
 from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from base64 import b64decode
-
-
-def zipFolder(p, pathFunc, zipf):
-    for base, dirs, files in walk(p):
-        if base.endswith('__pycache__'):
-            continue
-
-        for file in files:
-            if file.endswith('.tar'):
-                continue
-
-            fn = path.join(base, file)
-            zipf.write(fn, pathFunc(fn))
-
-
-def create_exp_dir(resultFolderPath):
-    # create folders
-    if not path.exists(resultFolderPath):
-        makedirs(resultFolderPath)
-
-    codeFilename = 'code.zip'
-    zipPath = '{}/{}'.format(resultFolderPath, codeFilename)
-    zipf = ZipFile(zipPath, 'w', ZIP_DEFLATED)
-
-    # init project base folder
-    baseFolder = path.dirname(path.abspath(getfile(currentframe())))  # script directory
-    # init path function
-    pathFunc = lambda fn: path.relpath(fn, baseFolder)
-    # init folders we want to zip
-    foldersToZip = ['models']
-    # save folders files
-    for folder in foldersToZip:
-        zipFolder('{}/{}'.format(baseFolder, folder), pathFunc, zipf)
-
-    # save main folder files
-    foldersToZip = ['.']
-    for folder in foldersToZip:
-        folderName = '{}/{}'.format(baseFolder, folder)
-        for file in listdir(folderName):
-            filePath = '{}/{}'.format(folderName, file)
-            if path.isfile(filePath):
-                zipf.write(filePath, pathFunc(filePath))
-
-    # close zip file
-    zipf.close()
-    # return zipPath, codeFilename
 
 
 # msg - email message, MIMEMultipart() object
