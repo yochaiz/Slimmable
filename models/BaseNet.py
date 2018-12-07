c76efcd924db1ece1995ec7ea4af8157ca61ac3b
brianchmiel <pancho2787>
2018-12-07 11:30:55 +0200

    * added classes Block, Layer, ConvLayer * created initial class BaseNet

---
+++ models/BaseNet.py
@@ -0,0 +1,104 @@
+from abc import abstractmethod
+from math import floor
+
+from torch.nn import Module, ModuleList, Conv2d, BatchNorm2d
+from torch.nn.functional import conv2d
+
+from HtmlLogger import HtmlLogger
+
+
+# abstract class for model block
+class Block(Module):
+    @abstractmethod
+    def getLayers(self):
+        raise NotImplementedError('subclasses must override getLayers()!')
+
+
+# abstract class for model layer
+class Layer(Block):
+    @abstractmethod
+    def getAllWidths(self):
+        raise NotImplementedError('subclasses must override getAllWidths()!')
+
+    @abstractmethod
+    def nFilters(self):
+        raise NotImplementedError('subclasses must override nFilters()!')
+
+
+class ConvLayer(Layer):
+    def __init__(self, widthRatioList, in_planes, out_planes, kernel_size, stride):
+        super(ConvLayer, self).__init__()
+
+        # init conv2d module
+        self.conv = Conv2d(in_planes, out_planes, kernel_size, stride=stride, padding=floor(kernel_size / 2), bias=False)
+
+        # save number of filters options
+        self.nFiltersList = [int(x * out_planes) for x in widthRatioList]
+        # init current number of filters
+        self.nFiltersCurrIdx = 0
+        # init independent batchnorm module for number of filters
+        self.bn = ModuleList([BatchNorm2d(n) for n in self.nFiltersList])
+
+    def forward(self, x):
+        # narrow conv weights (i.e. filters) according to current nFilters
+        out = conv2d(x, self.conv.weight.narrow(0, 0, self.nFiltersList[self.nFiltersCurrIdx]),
+                     bias=self.conv.bias, stride=self.conv.stride, padding=self.conv.padding, dilation=self.conv.dilation, groups=self.conv.groups)
+        out = self.bn[self.nFiltersCurrIdx](out)
+        return out
+
+    def getLayers(self):
+        return [self]
+
+    def getAllWidths(self):
+        return self.nFiltersList
+
+    def nFilters(self):
+        return self.conv.out_channels
+
+
+class BaseNet(Module):
+    def buildLayersList(self):
+        layersList = []
+        for layer in self.layers:
+            layersList.extend(layer.getLayers())
+
+        return layersList
+
+    def __init__(self, args, initLayersParams):
+        super(BaseNet, self).__init__()
+        # init save folder
+        saveFolder = args.save
+        # init layers
+        self.layers = self.initLayers(initLayersParams)
+        # build mixture layers list
+        self.layersList = self.buildLayersList()
+
+        self.printToFile(saveFolder)
+
+    @abstractmethod
+    def initLayers(self, params):
+        raise NotImplementedError('subclasses must override initLayers()!')
+
+    @abstractmethod
+    def forward(self, x):
+        raise NotImplementedError('subclasses must override forward()!')
+
+    def printToFile(self, saveFolder):
+        logger = HtmlLogger(saveFolder, 'model')
+        logger.setMaxTableCellLength(1000)
+
+        layerIdxKey = 'Layer#'
+        nFiltersKey = 'Filters#'
+        widthsKey = 'Width'
+        layerArchKey = 'Layer Architecture'
+        alphasKey = 'Alphas distribution'
+
+        logger.createDataTable('Model architecture', [layerIdxKey, nFiltersKey, widthsKey, layerArchKey])
+        for layerIdx, layer in enumerate(self.layersList):
+            widths = layer.getAllWidths()
+
+            dataRow = {layerIdxKey: layerIdx, nFiltersKey: layer.nFilters(), widthsKey: [widths], layerArchKey: layer}
+            logger.addDataRow(dataRow)
+
+        # # log layers alphas distribution
+        # self.logDominantQuantizedOp(len(bitwidths), loggerFuncs=[lambda k, rows: logger.addInfoTable(alphasKey, rows)])
