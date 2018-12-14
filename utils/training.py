530116b958e3c48bf1e07d3c62e2503dda0f7af1
yochaiz <yochaizur@gmail.com>
2018-12-14 12:39:49 +0200

    * added TrainingData, an abstract base class * replaced _createTableWithRoundedValues() with _createDictWithRoundedValues() * added functions to return average value * added class TrainingOptimum to handle training optimum result per width

--- utils/training.py
+++ utils/training.py
@@ -13,10 +13,21 @@ class AvgrageMeter(object):
         self.avg = self.sum / self.cnt
 
 
-class TrainingStats:
+# abstract base class
+class TrainingData:
     nRoundDigits = 5
+    avgKey = 'Avg'
 
+    @staticmethod
+    # returns only average value from dict
+    def dictAvg(dict):
+        return dict[TrainingData.avgKey]
+
+
+class TrainingStats(TrainingData):
     def __init__(self, widthRatio):
+        super(TrainingStats, self).__init__()
+
         self._epochLoss = {}
         self._batchLoss = {}
         self._top1 = {}
@@ -26,36 +37,48 @@ class TrainingStats:
             self._epochLoss[ratio] = AvgrageMeter()
             self._top1[ratio] = AvgrageMeter()
 
-    def _createTableWithRoundedValues(self, dict, valueFunc=lambda x: x):
-        table = []
+    # create dict copy with rounded values, for better display
+    # in addition, add average value to dict
+    def _createDictWithRoundedValues(self, dict, valueFunc=lambda x: x):
+        newDict = {}
         sum = 0.0
         for k, v in dict.items():
             value = valueFunc(v)
-            table.append([k, round(value, self.nRoundDigits)])
+            newDict[k] = round(value, self.nRoundDigits)
             sum += value
         # add average
-        table.append(['Avg', round(sum / len(dict), self.nRoundDigits)])
+        dict[self.avgKey] = sum / len(dict)
+        newDict[self.avgKey] = round(dict[self.avgKey], self.nRoundDigits)
 
-        return table
+        return newDict
 
     def prec1(self):
-        return self._createTableWithRoundedValues(self._prec1)
+        return self._createDictWithRoundedValues(self._prec1)
 
     def batchLoss(self):
-        return self._createTableWithRoundedValues(self._batchLoss)
+        return self._createDictWithRoundedValues(self._batchLoss)
 
     def epochLoss(self):
-        return self._createTableWithRoundedValues(self._epochLoss, lambda v: v.avg)
+        return self._createDictWithRoundedValues(self._epochLoss, lambda v: v.avg)
+
+    # returns only average value from dict, i.e. returns float
+    def epochLossAvg(self):
+        return self.dictAvg(self._epochLoss)
 
     def top1(self):
-        return self._createTableWithRoundedValues(self._top1, lambda v: v.avg)
+        return self._createDictWithRoundedValues(self._top1, lambda v: v.avg)
 
+    # returns only average value from dict, i.e. returns float
+    def top1Avg(self):
+        return self.dictAvg(self._top1)
+
+    # update values for given ratio
     def update(self, ratio, logits, target, loss):
         n = logits.size(0)
-        prec1 = self.accuracy(logits, target)[0]
+        prec1 = self.accuracy(logits, target)[0].item()
         self._epochLoss[ratio].update(loss.item(), n)
-        self._top1[ratio].update(prec1.item(), n)
-        self._prec1[ratio] = prec1.item()
+        self._top1[ratio].update(prec1, n)
+        self._prec1[ratio] = prec1
         self._batchLoss[ratio] = loss.item()
 
     @staticmethod
@@ -72,3 +95,40 @@ class TrainingStats:
             correct_k = correct[:k].view(-1).float().sum(0)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
+
+
+class TrainingOptimum(TrainingData):
+    def __init__(self, widthList, tableHeaders):
+        super(TrainingOptimum, self).__init__()
+
+        self._tableHeaders = tableHeaders
+
+        self._opt = {}
+        # init with invalid values
+        for width in widthList + [self.avgKey]:
+            self._opt[width] = (-1.0, None)
+
+    # create table for display in HtmlLogger
+    def _toTable(self, currEpoch):
+        table = [[h] for h in self._tableHeaders]
+        for width, (acc, epoch) in self._opt.items():
+            values = [width, acc, epoch, currEpoch - epoch]
+            for innerTable, v in zip(table, values):
+                innerTable.append(v)
+
+        return table
+
+    # update optimum values according to current epoch dict
+    def update(self, dict, epoch):
+        # update optimal values for each width
+        for width, acc in dict.items():
+            optAcc, _ = self._opt[width]
+            if acc > optAcc:
+                self._opt[width] = (acc, epoch)
+
+        # return table
+        return self._toTable(epoch)
+
+    # returns if given epoch is average best & average optimum
+    def is_best(self, epoch):
+        return epoch == self._opt[self.avgKey][-1], self._opt[self.avgKey][0]
