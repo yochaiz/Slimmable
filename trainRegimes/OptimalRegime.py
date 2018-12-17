38005b24f7ba50153f93f8f0c3757bbc5db07c50
yochaiz <yochaizur@gmail.com>
2018-12-17 15:25:35 +0200

    * added derived class OptimalRegime

---
+++ trainRegimes/OptimalRegime.py
@@ -0,0 +1,17 @@
+from .regime import TrainRegime
+
+
+class OptimalRegime(TrainRegime):
+    def __init__(self, args, logger):
+        super(OptimalRegime, self).__init__(args, logger)
+
+        # update values
+        args.train_portion = 1.0
+        args.batch_size = 250
+
+    def train(self):
+        # train model weights
+        self.initialWeightsTraining(trainFolderName='init_weights_train')
+
+        # init logger data table
+        self.logger.createDataTable('Summary', self.colsMainLogger)
