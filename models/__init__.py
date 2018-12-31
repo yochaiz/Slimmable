5efd0432be7b873fee6a0d272b84a5f46261e47e
yochaiz <yochaizur@gmail.com>
2018-12-31 17:55:32 +0200

    * added ResNet-18 for Imagenet

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,4 @@
-from .ResNet18 import ResNet18 as resnet18
+from .ResNet18 import ResNet18_Cifar as resnet18_cifar10
+from .ResNet18 import ResNet18_Cifar as resnet18_cifar100
+from .ResNet18 import ResNet18_Imagenet as resnet18_imagenet
+
