ef4b4c21ba05634a7f9fadd6a7cbea126ca85a28
yochaiz <yochaizur@gmail.com>
2018-12-31 11:42:52 +0200

    * replaced labelsMap with PlotLabelData instances * added class PlotLabelData to save label attributes * added colormap for plot line and error bars line

--- utils/statistics.py
+++ utils/statistics.py
@@ -324,25 +324,24 @@ class Statistics:
         saveFile(self.plotsData, self.plotsDataFilePath)
 
     @staticmethod
-    def plotFlops(flopsData, funcs, labelsToConnect, labelsMap, fileName, saveFolder):
+    def plotFlops(flopsData, funcs, labelsToConnect, fileName, saveFolder):
         flopsFunc, accFunc, partitionFunc = funcs
         # create plots
-        plots = [FlopsStandardPlot(flopsData.keys(), xFunc=flopsFunc, yFunc=accFunc),
-                 FlopsAveragePlot(flopsData.keys(), flopsFunc, accFunc, labelsToConnect, labelsMap),
-                 FlopsAveragePlot3D(partitionFunc, accFunc, labelsToConnect, labelsMap),
-                 FlopsMaxAccuracyPlot(flopsData.keys(), flopsFunc, accFunc, labelsToConnect, labelsMap),
-                 MinFlopsPlot(flopsData.keys(), flopsFunc, accFunc, labelsToConnect, labelsMap)
+        plots = [FlopsStandardPlot(xFunc=flopsFunc, yFunc=accFunc),
+                 FlopsAveragePlot(flopsFunc, accFunc, labelsToConnect),
+                 FlopsAveragePlot3D(partitionFunc, accFunc, labelsToConnect),
+                 FlopsMaxAccuracyPlot(flopsFunc, accFunc, labelsToConnect),
+                 MinFlopsPlot(flopsFunc, accFunc, labelsToConnect)
                  ]
 
         # iterate 1st over non-integer keys
-        for label in flopsData.keys():
-            labelFlopsData = flopsData[label]
-            for checkpoint in labelFlopsData:
+        for labelData in flopsData:
+            for checkpoint in labelData.checkpoints():
                 for plot in plots:
-                    plot.addDataPoint(checkpoint, label)
+                    plot.addDataPoint(checkpoint)
 
             for plot in plots:
-                plot.plot(label)
+                plot.plot(labelData)
 
         # set plot properties
         for plot in plots:
@@ -352,11 +351,38 @@ class Statistics:
         Statistics.saveFigPDF([plot.fig for plot in plots], fileName, saveFolder)
 
 
+class PlotLabelData:
+    def __init__(self, legendStr, annotateStr, color=None):
+        self._legendString = legendStr
+        self._annotateString = annotateStr
+        self._color = color
+        self._checkpoints = []
+
+    def color(self):
+        return self._color
+
+    def legendString(self):
+        return self._legendString
+
+    def annotateString(self):
+        return self._annotateString
+
+    def checkpoints(self):
+        for checkpoint in self._checkpoints:
+            yield checkpoint
+
+    def addCheckpoint(self, checkpoint):
+        self._checkpoints.append(checkpoint)
+
+    def setColor(self, color):
+        self._color = color
+
+
 class FlopsPlot:
     accuracyFormat = '{:.2f}'
     _titleKey = 'title'
 
-    def __init__(self, title, nKeys, xFunc, yFunc):
+    def __init__(self, title, xFunc, yFunc):
         self.title = title
         # create standard flops plot
         fig, ax = plt.subplots(nrows=1, ncols=1)
@@ -364,11 +390,6 @@ class FlopsPlot:
 
         self.fig = fig
         self.ax = ax
-
-        # init colors
-        colormap = plt.cm.hot
-        self.colors = [colormap(i) for i in linspace(0.7, 0.0, len(nKeys))]
-        self.nextColorIdx = 0
 
         # init yMax, yMin
         self.yMax = 0.0
@@ -410,7 +431,7 @@ class FlopsPlot:
         return getattr(checkpoint, self._titleKey, None)
 
     @abstractmethod
-    def addDataPoint(self, checkpoint, label):
+    def addDataPoint(self, checkpoint):
         raise NotImplementedError('subclasses must override addDataPoint()!')
 
     def addStandardDataPoint(self, checkpoint):
@@ -440,9 +461,8 @@ class FlopsPlot:
 
     # plot label values
     def plot(self, label):
-        self.ax.plot(self.xValues, self.yValues, 'o', label=label, c=self.colors[self.nextColorIdx])
+        self.ax.plot(self.xValues, self.yValues, 'o', label=label.legendString(), c=label.color())
         self.plotSpecific(label)
-        self.nextColorIdx += 1
         self.resetPlot()
 
     @abstractmethod
@@ -451,20 +471,20 @@ class FlopsPlot:
 
 
 class FlopsPlotWithLine(FlopsPlot):
-    def __init__(self, title, nKeys, xFunc, yFunc, labelsToConnect, labelsMap):
-        super(FlopsPlotWithLine, self).__init__(title, nKeys, xFunc, yFunc)
+    def __init__(self, title, xFunc, yFunc, labelsToConnect):
+        super(FlopsPlotWithLine, self).__init__(title, xFunc, yFunc)
 
         # init connect line attributes
         self.lineStyle = '--'
-        self.lineColor = 'green'
-        self.lineErrorBarColor = 'lightgreen'
-        # save labels map
-        self.labelsMap = labelsMap
+        colorMap = plt.get_cmap('Greens')
+        self.lineColor = [colorMap(i) for i in linspace(1.0, 0.6, len(labelsToConnect))]
+        colorMap = plt.get_cmap('YlGn')
+        self.lineErrorBarColor = [colorMap(i) for i in linspace(0.5, 0.2, len(labelsToConnect))]
         # save labels to connect list
         self.labelsToConnect = labelsToConnect
         # save the line points data, in case we want to evaluate other points against the line,
         # i.e. we want to know what is the line y value in any x value
-        self.linePoints = [[]] * len(labelsToConnect)
+        self.linePoints = [[] for _ in range(len(labelsToConnect))]
 
     def connectLabel(self, label, x, y):
         # save labelsList indices where label exists
@@ -475,7 +495,7 @@ class FlopsPlotWithLine(FlopsPlot):
                 # connect points
                 if len(self.linePoints[idx]) > 0:
                     xPrev, yPrev = self.linePoints[idx][-1]
-                    self.ax.plot([xPrev, x], [yPrev, y], self.lineStyle, c=self.lineColor)
+                    self.ax.plot([xPrev, x], [yPrev, y], self.lineStyle, c=self.lineColor[idx])
                 # add point data to self.linePoints while keeping it sorted
                 insort_left(self.linePoints[idx], (x, y))
 
@@ -485,17 +505,17 @@ class FlopsPlotWithLine(FlopsPlot):
 class FlopsAveragePlot(FlopsPlotWithLine):
     # labelsToConnect is list of lists
     # each list contains labels we want to connect with dashed line
-    def __init__(self, nKeys, xFunc, yFunc, labelsToConnect, labelsMap):
+    def __init__(self, xFunc, yFunc, labelsToConnect):
         # init confidence interval of 1 std
         self.confidence = 0.6827
 
         title = 'Average accuracy vs. Flops | Confidence:[{}]'.format(self.confidence)
-        super(FlopsAveragePlot, self).__init__(title, nKeys, xFunc, yFunc, labelsToConnect, labelsMap)
+        super(FlopsAveragePlot, self).__init__(title, xFunc, yFunc, labelsToConnect)
 
         # save previous line point error bar, in order to connect also error bars with line
         self.prevErrorBar = [None] * len(labelsToConnect)
 
-    def _connectErrorBar(self, labelIndices, x, y, confidenceHalfInterval):
+    def _connectErrorBars(self, labelIndices, x, y, confidenceHalfInterval):
         # iterate over lines where label is part of
         for idx in labelIndices:
             prevError = self.prevErrorBar[idx]
@@ -503,13 +523,15 @@ class FlopsAveragePlot(FlopsPlotWithLine):
             if prevError is not None:
                 xPrev, yPrev, confidenceHalfIntervalPrev = prevError
                 # connect bar bottom
-                self.ax.plot([xPrev, x], [yPrev - confidenceHalfIntervalPrev, y - confidenceHalfInterval], self.lineStyle, c=self.lineErrorBarColor)
+                self.ax.plot([xPrev, x], [yPrev - confidenceHalfIntervalPrev, y - confidenceHalfInterval],
+                             self.lineStyle, c=self.lineErrorBarColor[idx])
                 # connect bar top
-                self.ax.plot([xPrev, x], [yPrev + confidenceHalfIntervalPrev, y + confidenceHalfInterval], self.lineStyle, c=self.lineErrorBarColor)
+                self.ax.plot([xPrev, x], [yPrev + confidenceHalfIntervalPrev, y + confidenceHalfInterval],
+                             self.lineStyle, c=self.lineErrorBarColor[idx])
             # save last point as previous point
             self.prevErrorBar[idx] = (x, y, confidenceHalfInterval)
 
-    def addDataPoint(self, checkpoint, label):
+    def addDataPoint(self, checkpoint):
         flops = self.xFunc(checkpoint)
         accuracy = self.yFunc(checkpoint)
         # title = self.getTitle(checkpoint)
@@ -523,7 +545,7 @@ class FlopsAveragePlot(FlopsPlotWithLine):
         self.yValues.append(accuracy)
 
     # add error bar to point average
-    def plotErrorBar(self, yMean, checkConditionsFlag):
+    def plotErrorBar(self, yMean, color, checkConditionsFlag):
         confidenceHalfInterval = 0
         if len(self.yValues) > 1:
             # calc standard error of the mean
@@ -537,18 +559,19 @@ class FlopsAveragePlot(FlopsPlotWithLine):
                 for pointsList in self.linePoints:
                     # find current point line interval
                     upperBoundIdx = bisect_left(pointsList, (xMean, yMean))
-                    lowerBoundIdx = upperBoundIdx - 1
-                    # calc line slope
-                    xUpper, yUpper = pointsList[upperBoundIdx]
-                    xLower, yLower = pointsList[lowerBoundIdx]
-                    slope = (yUpper - yLower) / (xUpper - xLower)
-                    # calc line y value in xMean
-                    yLine = yLower + (slope * (xMean - xLower))
-                    # do not plot error bar if error bar lower bound is under line y value in xMean
-                    if yLine > (yMean - confidenceHalfInterval):
-                        return
+                    if upperBoundIdx > 0:
+                        lowerBoundIdx = upperBoundIdx - 1
+                        # calc line slope
+                        xUpper, yUpper = pointsList[upperBoundIdx]
+                        xLower, yLower = pointsList[lowerBoundIdx]
+                        slope = (yUpper - yLower) / (xUpper - xLower)
+                        # calc line y value in xMean
+                        yLine = yLower + (slope * (xMean - xLower))
+                        # do not plot error bar if error bar lower bound is under line y value in xMean
+                        if yLine > (yMean - confidenceHalfInterval):
+                            return
 
-            self.ax.errorbar(self.xValues, [yMean], yerr=confidenceHalfInterval, c=self.colors[self.nextColorIdx],
+            self.ax.errorbar(self.xValues, [yMean], yerr=confidenceHalfInterval, c=color,
                              ms=5, marker='X', capsize=4, markeredgewidth=1, elinewidth=2)
 
         return confidenceHalfInterval
@@ -557,37 +580,36 @@ class FlopsAveragePlot(FlopsPlotWithLine):
     def plot(self, label):
         # average accuracy
         yMean = mean(self.yValues)
-        self.ax.plot(self.xValues, [yMean], 'o', label=label, c=self.colors[self.nextColorIdx])
+        self.ax.plot(self.xValues, [yMean], 'o', label=label.legendString(), c=label.color())
         # annotate label
-        self.ax.annotate(self.labelsMap[label], (self.xValues[0], yMean), size=6)
+        self.ax.annotate(label.annotateString(), (self.xValues[0], yMean), size=6)
 
         # update yMax, yMin
         self.yMax = max(self.yMax, yMean)
         self.yMin = min(self.yMin, yMean)
 
         # connect label
-        labelIndices = self.connectLabel(label, self.xValues[-1], yMean)
+        labelIndices = self.connectLabel(label.legendString(), self.xValues[-1], yMean)
         # plot error bar
-        confidenceHalfInterval = self.plotErrorBar(yMean, checkConditionsFlag=(len(labelIndices) == 0))
+        confidenceHalfInterval = self.plotErrorBar(yMean, label.color(), checkConditionsFlag=(len(labelIndices) == 0))
         # connect error bars if we have connected label
-        self._connectErrorBar(labelIndices, self.xValues[0], yMean, confidenceHalfInterval)
+        self._connectErrorBars(labelIndices, self.xValues[0], yMean, confidenceHalfInterval)
 
         # update variables for next plot
-        self.nextColorIdx += 1
         self.resetPlot()
 
 
 class FlopsAveragePlot3D(FlopsAveragePlot):
     # labelsToConnect is list of lists
     # each list contains labels we want to connect with dashed line
-    def __init__(self, xFunc, yFunc, labelsToConnect, labelsMap):
+    def __init__(self, xFunc, yFunc, labelsToConnect):
         # init accuracy color range
         self.minColorValue = 82.0
         self.maxColorValue = 89.0
         nKeys = 81
         self.colorsValueList = linspace(self.minColorValue, self.maxColorValue, nKeys)
 
-        super(FlopsAveragePlot3D, self).__init__(self.colorsValueList, xFunc, yFunc, labelsToConnect, labelsMap)
+        super(FlopsAveragePlot3D, self).__init__(xFunc, yFunc, labelsToConnect)
 
         self.ax = self.fig.add_subplot(1, 1, 1, projection='3d')
 
@@ -626,19 +648,18 @@ class FlopsAveragePlot3D(FlopsAveragePlot):
         # average accuracy
         yMean = mean(self.yValues)
         # plot 3D, the color is best on accuracy, i.e. yMean
-        self.ax.plot([x1], [x2], [x3], 'o', label=label, c=self.valueToColor(yMean))
+        self.ax.plot([x1], [x2], [x3], 'o', label=label.legendString(), c=self.valueToColor(yMean))
         # annotate label
-        # self.ax.annotate(self.labelsMap[label], (self.xValues[0], yMean), size=6)
-        self.ax.text(x1, x2, x3, self.labelsMap[label], size=6, zorder=1)
+        self.ax.text(x1, x2, x3, label.annotateString(), size=6, zorder=1)
 
         self.resetPlot()
 
 
 class FlopsStandardPlot(FlopsPlot):
-    def __init__(self, nKeys, xFunc, yFunc):
-        super(FlopsStandardPlot, self).__init__('Accuracy vs. Flops', nKeys, xFunc, yFunc)
+    def __init__(self, xFunc, yFunc):
+        super(FlopsStandardPlot, self).__init__('Accuracy vs. Flops', xFunc, yFunc)
 
-    def addDataPoint(self, checkpoint, label):
+    def addDataPoint(self, checkpoint):
         self.addStandardDataPoint(checkpoint)
 
     def plotSpecific(self, label):
@@ -646,14 +667,14 @@ class FlopsStandardPlot(FlopsPlot):
 
 
 class FlopsPlotWithCondition(FlopsPlotWithLine):
-    def __init__(self, title, nKeys, xFunc, yFunc, labelsToConnect, labelsMap):
-        super(FlopsPlotWithCondition, self).__init__(title, nKeys, xFunc, yFunc, labelsToConnect, labelsMap)
+    def __init__(self, title, xFunc, yFunc, labelsToConnect):
+        super(FlopsPlotWithCondition, self).__init__(title, xFunc, yFunc, labelsToConnect)
 
     @abstractmethod
     def condition(self, checkpoint):
         raise NotImplementedError('subclasses must override condition()!')
 
-    def addDataPoint(self, checkpoint, label):
+    def addDataPoint(self, checkpoint):
         flops = self.xFunc(checkpoint)
         accuracy = self.yFunc(checkpoint)
 
@@ -671,14 +692,14 @@ class FlopsPlotWithCondition(FlopsPlotWithLine):
             flops = self.xValues[0] if len(self.xValues) > 0 else None
 
             # annotate label
-            self.ax.annotate(self.labelsMap[label], (flops, accuracy), size=6)
+            self.ax.annotate(label.annotateString(), (flops, accuracy), size=6)
             # connect label
             self.connectLabel(label, self.xValues[0], self.yValues[0])
 
 
 class FlopsMaxAccuracyPlot(FlopsPlotWithCondition):
-    def __init__(self, nKeys, xFunc, yFunc, labelsToConnect, labelsMap):
-        super(FlopsMaxAccuracyPlot, self).__init__('Max accuracy vs. Flops', nKeys, xFunc, yFunc, labelsToConnect, labelsMap)
+    def __init__(self, xFunc, yFunc, labelsToConnect):
+        super(FlopsMaxAccuracyPlot, self).__init__('Max accuracy vs. Flops', xFunc, yFunc, labelsToConnect)
 
     def condition(self, checkpoint):
         accuracy = self.yFunc(checkpoint)
@@ -686,8 +707,8 @@ class FlopsMaxAccuracyPlot(FlopsPlotWithCondition):
 
 
 class MinFlopsPlot(FlopsPlotWithCondition):
-    def __init__(self, nKeys, xFunc, yFunc, labelsToConnect, labelsMap):
-        super(MinFlopsPlot, self).__init__('Accuracy vs. Min Flops', nKeys, xFunc, yFunc, labelsToConnect, labelsMap)
+    def __init__(self, xFunc, yFunc, labelsToConnect):
+        super(MinFlopsPlot, self).__init__('Accuracy vs. Min Flops', xFunc, yFunc, labelsToConnect)
 
     def condition(self, checkpoint):
         flops = self.xFunc(checkpoint)
