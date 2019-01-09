834d1000361404eccf99a52b5c0a94b805e822c6
yochaiz <yochaizur@gmail.com>
2019-01-09 18:14:24 +0200

    * added PreTrainedRegime

--- trainRegimes/__init__.py
+++ trainRegimes/__init__.py
@@ -1,3 +1,4 @@
 from .regime import TrainRegime
 from .SearchRegime import SearchRegime as search
 from .OptimalRegime import OptimalRegime as optimal
+from .PreTrainedRegime import PreTrainedRegime as pre_trained
