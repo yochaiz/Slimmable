53b2c359890efd5433276e02059080b20ce14afe
yochaiz <yochaizur@gmail.com>
2019-01-17 11:53:21 +0200

    * modified imports due to changes in BaseNet class

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,14 @@
-from .ResNet18 import ResNet18_Cifar as resnet18_cifar10
-from .ResNet18 import ResNet18_Cifar as resnet18_cifar100
-from .ResNet18 import ResNet18_Imagenet as resnet18_imagenet
+_modelsNames = ['resnet18_cifar10', 'resnet18_cifar100', 'resnet18_imagenet']
 
+
+def getModelNames():
+    return _modelsNames
+
+
+def getModelDict():
+    from .ResNet18 import ResNet18_Cifar as resnet18_cifar10
+    from .ResNet18 import ResNet18_Cifar as resnet18_cifar100
+    from .ResNet18 import ResNet18_Imagenet as resnet18_imagenet
+
+    models = [resnet18_cifar10, resnet18_cifar100, resnet18_imagenet]
+    return {k: v for k, v in zip(_modelsNames, models)}
