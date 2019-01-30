ebb2c7551f14f74c5ef39aac95b2939fad1db78b
yochaiz <yochaizur@gmail.com>
2019-01-30 12:52:33 +0200

    * renamed self.layerPaths to self.trainPaths

--- replicator/TrainPathWeights.py
+++ replicator/TrainPathWeights.py
@@ -6,8 +6,8 @@ class TrainPathWeights(TrainWeights):
     def __init__(self, getModel, getModelParallel, getArgs, getLogger, getTrainQueue, getValidQueue, getTrainFolderPath, gpu):
         super(TrainPathWeights, self).__init__(getModel, getModelParallel, getArgs, getLogger, getTrainQueue, getValidQueue, getTrainFolderPath)
 
-        # init layer paths dictionary
-        self.layerPaths = {}
+        # init train paths dictionary
+        self.trainPaths = {}
         # save GPU device number
         self._gpu = gpu
 
@@ -15,11 +15,11 @@ class TrainPathWeights(TrainWeights):
         return epoch >= 5
 
     def widthList(self):
-        return self.layerPaths.items()
+        return self.trainPaths.items()
 
     def train(self, paths: dict):
         # update training paths
-        self.layerPaths = paths
+        self.trainPaths = paths
         # init optimizer
         optimizer = self._initOptimizer()
         # train
