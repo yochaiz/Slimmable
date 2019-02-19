29d9618f7f2c7a0e7ba7457c7a79b6483e5b1284
yochaiz <yochaizur@gmail.com>
2019-02-19 19:48:39 +0200

    * changed --search_epochs default value

--- utils/args.py
+++ utils/args.py
@@ -97,7 +97,7 @@ def parseArgs():
     parser.add_argument('--pre_trained', type=str, default=None, help='pre-trained model to copy weights from')
     parser.add_argument('--{}'.format(modelFlopsKey), type=str, default=None, help='model flops list where each element is a layer flops dict')
     # training params
-    parser.add_argument('--search_epochs', type=int, default=100, help='number of search regime epochs')
+    parser.add_argument('--search_epochs', type=int, default=1000, help='number of search regime epochs')
     parser.add_argument('--weights_epochs', type=int, default=300, help='number of weights training epochs')
     # parser.add_argument('--train_portion', type=float, default=1.0, help='portion of training data')
     # parser.add_argument('--train_regime', default='TrainRegime', choices=trainRegimesNames, help='Training regime')
