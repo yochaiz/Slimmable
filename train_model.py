2026303176f37ce5512f4a32be3ce230c024fc61
yochaiz <yochaizur@gmail.com>
2019-03-04 16:06:37 +0200

    * made changed to train checkpoints in a given folder

--- train_model.py
+++ train_model.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 from datetime import datetime
 from numpy import random
+from shutil import copy2
+from os import listdir, remove
 from os.path import dirname, basename, exists
 
 from torch import load as loadCheckpoint
@@ -18,9 +20,13 @@ from utils.zip import create_exp_dir
 from utils.checkpoint import checkpointFileType
 
 
+def checkpointPrefix(fileName):
+    return fileName[:-len(checkpointFileType) - 1]
+
+
 def train(scriptArgs):
     # load args from file
-    args = loadCheckpoint(scriptArgs.json, map_location=lambda storage, loc: storage.cuda())
+    args = loadCheckpoint('{}/{}'.format(scriptArgs.folderPath, scriptArgs.json), map_location=lambda storage, loc: storage.cuda())
 
     # terminate if validAcc exists
     _validAccKey = TrainWeights.validAccKey
@@ -50,13 +56,14 @@ def train(scriptArgs):
     if modelFlopsPath and exists(modelFlopsPath):
         setattr(args, BaseNet.modelFlopsKey(), loadCheckpoint(modelFlopsPath))
 
-    # extract args JSON folder path
-    folderName = dirname(scriptArgs.json)
-    # results folder is JSON filename
-    jsonFileName = basename(scriptArgs.json)
+    # # extract args JSON folder path
+    # folderName = dirname(scriptArgs.json)
+    # # results folder is JSON filename
+    # jsonFileName = basename(scriptArgs.json)
     # set results folder path
-    args.save = '{}/{}'.format(folderName, jsonFileName[:-len(checkpointFileType) - 1])
-    if not exists(args.save):
+    args.save = '{}/{}'.format(scriptArgs.folderPath, checkpointPrefix(scriptArgs.json))
+    folderNotExists = not exists(args.save)
+    if folderNotExists:
         create_exp_dir(args.save)
         # init logger
         logger = HtmlLogger(args.save, 'log')
@@ -68,14 +75,45 @@ def train(scriptArgs):
         # train according to chosen regime
         alphasRegime.train()
 
+    return folderNotExists
+
+
+def iterateFolder(scriptArgs):
+    foundCheckpointToTrain = True
+    while foundCheckpointToTrain:
+        foundCheckpointToTrain = False
+        checkpointsList = sorted(listdir(scriptArgs.folderPath), reverse=True)
+        for file in checkpointsList:
+            if file.endswith('].{}'.format(checkpointFileType)):
+                # init folder name
+                prefix = checkpointPrefix(file)
+                folderName = '{}-{}'.format(prefix, scriptArgs.repeatNum)
+                # init checkpoint folder path
+                folderPath = '{}/{}'.format(scriptArgs.folderPath, folderName)
+                if not exists(folderPath):
+                    # init new checkpoint file name
+                    scriptArgs.json = '{}.{}'.format(folderName, checkpointFileType)
+                    # init original & new checkpoints paths
+                    filePath = '{}/{}'.format(scriptArgs.folderPath, file)
+                    jsonPath = '{}/{}'.format(scriptArgs.folderPath, scriptArgs.json)
+                    # copy checkpoint
+                    copy2(filePath, jsonPath)
+                    # train checkpoint
+                    trainedCheckpoint = train(scriptArgs)
+                    foundCheckpointToTrain = foundCheckpointToTrain or trainedCheckpoint
+                    # remove original checkpoint
+                    remove(file)
+
 
 if not is_available():
     print('no gpu device available')
     exit(1)
 
 parser = ArgumentParser()
-parser.add_argument('--json', type=str, required=True, help='JSON file path')
+# parser.add_argument('--json', type=str, required=True, help='JSON file path')
+parser.add_argument('--folderPath', type=str, required=True, help='checkpoints folder path')
 parser.add_argument('--data', type=str, required=True, help='datasets folder path')
+parser.add_argument('--repeatNum', type=int, required=True, choices=range(1, 100), help='checkpoint training repeat number')
 parser.add_argument('--gpu', type=str, default='0', help='gpu device id, e.g. 0,1,3')
 parser.add_argument('--workers', type=int, default=0, choices=range(0, 32), help='num of workers')
 parser.add_argument('--optimal_epochs', type=int, default=150, help='stop training weights if there is no new optimum in last optimal_epochs')
@@ -87,4 +125,4 @@ scriptArgs = parser.parse_args()
 if type(scriptArgs.gpu) is str:
     scriptArgs.gpu = [int(i) for i in scriptArgs.gpu.split(',')]
 
-train(scriptArgs)
+iterateFolder(scriptArgs)
